a01074ad120e17806fccde1443d10053608aa7a7
L Odette <wallyjulian@gmail.com>
2025-11-15 18:30:55 -0500

    feat: week 8

---
+++ labs/R/rubrics.R
@@ -0,0 +1,70 @@
+# created November 14, 2025
+
+
+# rubric default ----
+tibble::tibble(
+  exercise = "1-10"
+  , points = "3 points each"
+) |>
+  gt::gt() |>
+  gt::cols_align(
+    align = "left",
+    columns = exercise
+  ) |>
+  gt::cols_width(exercise ~ gt::px(250), points ~ gt::px(150)) |>
+  gt::tab_footnote(
+    footnote = gt::md("code (if any) does not run to completion: **-1 point**")
+    , locations = gt::cells_column_labels(points)
+  ) |>
+  gt::tab_footnote(
+    footnote = gt::md("explicit question (if any) not answered, even if code runs to completion: **-1 point**")
+    , locations = gt::cells_column_labels(points)
+  ) |>
+  gtExtras::gt_theme_espn()
+
+
+
+# rubric 8 ----
+tibble::tibble(
+  exercise = c("1-3, 6-8", "4-5")
+  , points = c("3 points each", "6 points each")
+) |>
+  gt::gt() |>
+  gt::cols_align(
+    align = "left",
+    columns = exercise
+  ) |>
+  gt::cols_width(exercise ~ gt::px(250), points ~ gt::px(150)) |>
+  gt::tab_footnote(
+    footnote = gt::md("code (if any) does not run to completion: **-1 point**")
+    , locations = gt::cells_column_labels(points)
+  ) |>
+  gt::tab_footnote(
+    footnote = gt::md("explicit question (if any) not answered, even if code runs to completion: **-1 point**")
+    , locations = gt::cells_column_labels(points)
+  ) |>
+  gtExtras::gt_theme_espn() |>
+  gt::gtsave("labs/images/rubric_lab_8.png")
+
+
+# rubric midterm ----
+tibble::tibble(
+  question = c("1-10", "11", "12", "13", "14")
+  , points = c("1 point each", "4 points", "5 points", "3 points", "3 points")
+) |>
+  gt::gt() |>
+  gt::cols_align(
+    align = "left",
+    columns = question
+  ) |>
+  gt::cols_width(question ~ gt::px(250), points ~ gt::px(150)) |>
+  gt::tab_footnote(
+    footnote = gt::md("code (if any) does not run to completion: **-1 point**")
+    , locations = gt::cells_column_labels(points)
+  ) |>
+  gt::tab_footnote(
+    footnote = gt::md("explicit question (if any) not answered, even if code runs to completion: **-1 point**")
+    , locations = gt::cells_column_labels(points)
+  ) |>
+  gtExtras::gt_theme_espn() |>
+  gt::gtsave("labs/images/rubric_midterm.png")
