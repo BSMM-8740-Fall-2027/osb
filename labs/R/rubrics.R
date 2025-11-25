f5b3b7fc59ccc98b248fb2380c78e120499af400
L Odette <wallyjulian@gmail.com>
2025-11-25 12:04:58 -0500

    feat week 9

--- labs/R/rubrics.R
+++ labs/R/rubrics.R
@@ -26,7 +26,7 @@ tibble::tibble(
 
 # rubric 8 ----
 tibble::tibble(
-  exercise = c("1-3, 6-8", "4-5")
+  exercise = c("1-2, 5-8", "3-4")
   , points = c("3 points each", "6 points each")
 ) |>
   gt::gt() |>
@@ -45,6 +45,28 @@ tibble::tibble(
   ) |>
   gtExtras::gt_theme_espn() |>
   gt::gtsave("labs/images/rubric_lab_8.png")
+
+# rubric 9 ----
+tibble::tibble(
+  exercise = "1-6"
+  , points = "5 points each"
+) |>
+  gt::gt() |>
+  gt::cols_align(
+    align = "left",
+    columns = exercise
+  ) |>
+  gt::cols_width(exercise ~ gt::px(250), points ~ gt::px(150)) |>
+  gt::tab_footnote(
+    footnote = gt::md("code (if any) does not run to completion: **-1 point**")
+    , locations = gt::cells_column_labels(points)
+  ) |>
+  gt::tab_footnote(
+    footnote = gt::md("explicit question (if any) not answered, even if code runs to completion: **-1 point**")
+    , locations = gt::cells_column_labels(points)
+  ) |>
+  gtExtras::gt_theme_espn() |>
+  gt::gtsave("labs/images/rubric_lab_9.png")
 
 
 # rubric midterm ----
